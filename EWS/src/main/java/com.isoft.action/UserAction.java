8b363a82741c01ad0e5f898ac300fde72394ffac
wlyxh <1440812482@qq.com>
2019-05-21 17:05:04 +0800

    asd

--- EWS/src/main/java/com.isoft.action/UserAction.java
+++ EWS/src/main/java/com.isoft.action/UserAction.java
@@ -1,5 +1,8 @@
 package com.isoft.action;
 
+import com.isoft.service.IUserService;
+import com.isoft.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,22 +11,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/user")
 
 public class UserAction {
+    @Autowired
+    IUserService userService;
     @RequestMapping("/login.do")
-
     @ResponseBody
     public String login(String uname, String upwd) {
         /*硬编码*/
-        if (uname.equals("admin") && upwd.equals("123456"))
-            return "success";
-        else
-            return "fault";
+        String str=userService.login(uname,upwd);
+       return str;
     }
+
     @RequestMapping("/register.do")
     @ResponseBody
     public String register(String uname, String upwd,String email) {
         /*硬编码*/
-        boolean t=true;
-        if (t)
+        boolean temp=userService.register(uname,upwd,email);
+        if (temp)
             return "success";
         else
             return "fault";
